8fecd4a30f845fa56b60899ddb600766e894edec
smartwalle <smartwalle@gmail.com>
2020-06-11 15:33:49 +0800

    fmt

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -12,37 +12,37 @@ const (
 	kPrefix = "lock"
 )
 
-type etcdSession struct {
+type session struct {
 	key    string
 	client *clientv3.Client
 	opts   []concurrency.SessionOption
 }
 
-func (this *etcdSession) NewMutex(key string) lock.Mutex {
-	var nPath = path.Join(kPrefix, this.key, key)
-	var session, err = concurrency.NewSession(this.client, this.opts...)
-	var mu = &etcdMutex{}
-	mu.err = err
-	mu.session = session
-	mu.mu = concurrency.NewMutex(session, nPath)
-	return mu
-}
-
 func NewSession(key string, client *clientv3.Client, opts ...concurrency.SessionOption) lock.Session {
-	var s = &etcdSession{}
+	var s = &session{}
 	s.key = key
 	s.client = client
 	s.opts = opts
 	return s
 }
 
-type etcdMutex struct {
+func (this *session) NewMutex(key string) lock.Mutex {
+	var nPath = path.Join(kPrefix, this.key, key)
+	var session, err = concurrency.NewSession(this.client, this.opts...)
+	var mu = &mutex{}
+	mu.err = err
+	mu.session = session
+	mu.mu = concurrency.NewMutex(session, nPath)
+	return mu
+}
+
+type mutex struct {
 	err     error
 	session *concurrency.Session
 	mu      *concurrency.Mutex
 }
 
-func (this *etcdMutex) Lock() error {
+func (this *mutex) Lock() error {
 	if this.err != nil {
 		return this.err
 	}
@@ -54,7 +54,7 @@ func (this *etcdMutex) Lock() error {
 	return err
 }
 
-func (this *etcdMutex) Unlock() error {
+func (this *mutex) Unlock() error {
 	var err error
 	if this.mu != nil {
 		err = this.mu.Unlock(context.TODO())
--- zookeeper/zookeeper.go
+++ zookeeper/zookeeper.go
@@ -10,21 +10,21 @@ const (
 	kPrefix = "/lock/"
 )
 
-type zookeeperSession struct {
+type session struct {
 	key  string
 	conn *zk.Conn
 	acl  []zk.ACL
 }
 
-func (this *zookeeperSession) NewMutex(key string) lock.Mutex {
-	var nPath = path.Join(kPrefix, this.key, key)
-	return zk.NewLock(this.conn, nPath, this.acl)
-}
-
 func NewSession(key string, conn *zk.Conn, acl []zk.ACL) lock.Session {
-	var s = &zookeeperSession{}
+	var s = &session{}
 	s.key = key
 	s.conn = conn
 	s.acl = acl
 	return s
+}
+
+func (this *session) NewMutex(key string) lock.Mutex {
+	var nPath = path.Join(kPrefix, this.key, key)
+	return zk.NewLock(this.conn, nPath, this.acl)
 }
